39591d4dd94c3966593a8a41ba73dae4179fef02
Anna-Lena Marx <mail@marx.engineer>
2025-12-28 16:53:03 +0100

    feat: add the board definition for the ZEReader rev2 PCB (#28)
    The second revision of the ZEReader PCB comes with some
    breaking changes in the device tree.
    Several pin configurations changed, and with the new
    latching (self-holding) on/off button circuitry,
    additional device tree configurations are added.
    For turning the device off, ZEReader takes advantage
    of the "zephyr,input-longpress" infrastructure to allow
    a customizable time the button must be pressed before
    the device turned off.
    The code makes use of Zephyr's KConfig system to ensure
    both hardware revisions stay usable and supported.
    The on/off related code is disabled for rev1.
    
    Signed-off-by: Anna-Lena Marx <mail@marx.engineer>

--- app/src/main.c
+++ app/src/main.c
@@ -9,23 +9,42 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/input/input.h>
 
 #include <epub/epub.h>
 #include <ui/ui.h>
 
+#define POWERCONTROL DT_ALIAS(devicestatecontrol)
+
 LOG_MODULE_REGISTER(main, CONFIG_ZEREADER_LOG_LEVEL);
+
+#if defined(CONFIG_BUTTON_ONOFF)
+static const struct device *const longpress_dev = DEVICE_DT_GET(DT_NODELABEL(longpress));
+static const struct gpio_dt_spec powercontrol_pin = GPIO_DT_SPEC_GET(POWERCONTROL, gpios);
+
+static void longpress_shutdown_cb(struct input_event *event, void *user_data)
+{
+	LOG_DBG("Long press detected - turning the device off!");
+	gpio_pin_set_dt(&powercontrol_pin, 0);
+}
+INPUT_CALLBACK_DEFINE(longpress_dev, longpress_shutdown_cb, NULL);
+
+#endif // CONFIG_BUTTON_ONOFF
 
 int main(void)
 {
-	LOG_DBG("ZEReader started! %s\n", CONFIG_BOARD_TARGET);
+	int error = 0;
+
+	LOG_DBG("ZEReader is starting... %s\n", CONFIG_BOARD_TARGET);
 
 	// Initialize the choosen zephyr,display device
-	// -> Make the device tree description available for the software part
+	// and make the device tree description available for the software part.
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	if (!device_is_ready(display_dev))
 	{
-		LOG_ERR("Device not ready, aborting...");
+		LOG_ERR("Device 'display_dev' is not ready, aborting...");
 		return 0;
 	}
 
@@ -34,24 +53,58 @@ int main(void)
 	lvgl_btn_dev = DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_button_input));
 	if (!device_is_ready(lvgl_btn_dev))
 	{
-		LOG_ERR("Device not ready, aborting...");
+		LOG_ERR("Device 'lvgl_btn_dev' is not ready, aborting...");
 		return 0;
 	}
+
+#if defined(CONFIG_BUTTON_ONOFF)
+	LOG_DBG("Button On/Off configured!");
+	if (!gpio_is_ready_dt(&powercontrol_pin))
+	{
+		LOG_ERR("Device 'powercontrol_pin' is not ready, aborting...");
+		return 0;
+	}
+	error = gpio_pin_configure_dt(&powercontrol_pin, GPIO_OUTPUT_ACTIVE);
+	if (error < 0)
+	{
+		LOG_ERR("Device 'powercontrol_pin' could not be configured as an output!");
+		return error;
+	}
+
+	if (!device_is_ready(longpress_dev))
+	{
+		LOG_ERR("Device 'longpress_dev' is not ready, aborting...");
+		return 0;
+	}
+#endif // CONFIG_BUTTON_ONOFF
 
 	context_t context = READING;
 
 	zereader_setup_page();
 	zereader_setup_control_buttons(&context);
+	epub_initialize();
+
+#if defined(CONFIG_BUTTON_ONOFF)
+	error = gpio_pin_set_dt(&powercontrol_pin, 1);
+	if (error < 0)
+	{
+		LOG_ERR("Unable to set 'powercontrol_pin'!");
+		return error;
+	}
+	else
+	{
+		LOG_DBG("ZEReader is now ON!");
+	}
+#endif // CONFIG_BUTTON_ONOFF
+
 	zereader_show_logo();
 	lv_timer_handler();
 	display_blanking_off(display_dev);
-
-	epub_initialize();
-	int err = epub_restore_book();
-
 	zereader_clean_page();
 
-	if (err == 0) {
+	error = epub_restore_book();
+	if (error == 0)
+	{
 		zereader_print_current_page();
 	}
 
